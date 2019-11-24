074d93026bb70581d9d1dbe5ae096fc32e38ec28
Felipe Schramm <felipecschramm@hotmail.com>
2019-11-24 19:29:52 -0300

    Backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const routes = require('./routes');
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -10,7 +11,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require("./routes"));
+app.use("/api", routes)
 
 app.listen(process.env.PORT || 3000);
 
