58893974bd20fa88f76a6d3d9ae15ad9c513e201
Felipe Schramm <felipecschramm@hotmail.com>
2019-12-09 17:59:32 -0300

    Ultimo back

--- src/routes.js
+++ src/routes.js
@@ -3,30 +3,54 @@ const Atividade = require("./models/AtividadeSchema");
 const Usuario = require("./models/UsuarioSchema");
 
 routes.get("/home", async (req, res) => {
+
+  const atividades = await Atividade.find();
+
+  let semana = new Array(24);
+  for (let i = 0; i < semana.length; i++) {
+    semana[i] = new Array(7);
+  }
+
+  atividades.forEach(function (ativ) {
+    let aux = new Date(ativ.data);
+
+    if (semana[aux.getHours()][aux.getDay()] !== undefined)
+      semana[aux.getHours()][aux.getDay()].push(ativ.nome);
+    else {
+      semana[aux.getHours()][aux.getDay()] = [];
+      semana[aux.getHours()][aux.getDay()].push(ativ);
+    }
+  })
+  return res.json(semana);
+
+})
+
+routes.get("/listar", async (req, res) => {
   const atividade = await Atividade.find();
   return res.json(atividade);
 });
 
 routes.post("/ativ", async (req, res) => {
-  const atividade = await Atividade.create(req.body);
+  const atividade = await Atividade.create({
+    'nome': req.body.nome,
+    'data': req.body.data,
+    'descricao': req.body.descricao
+  });
   return res.json(atividade);
 });
 
 routes.get("/ativ/:nome", async (req, res) => {
-  const atividade = await Atividade.find({'nome': req.params.nome});
-
+  const atividade = await Atividade.find({
+    nome: new RegExp(`^${req.params.nome}`, 'i')
+  });
   return res.json(atividade);
 });
 
-routes.get("/user", async (req, res) => {
-  const usuario = await Usuario.find();
-
-  return res.json(atividade);
-});
-
-routes.post("/user/login", async (req, res) => {
-  const usuario = await Usuario.find({ 'login': req.body.login })
-    
+routes.post("/user/:login", async (req, res) => {
+  const usuario = await Usuario.findOne({
+    'login': req.params.login,
+    'senha': req.body.senha
+  })
   return res.json(usuario)
 })
 
@@ -38,5 +62,11 @@ routes.post("/user", async (req, res) => {
   return res.json(usuario)
 })
 
-module.exports = routes;
 
+// routes.get("/users", async (req, res) => {
+//   const usuarios = await Usuario.find()
+
+//   return res.json(usuarios)
+// })
+
+module.exports = routes;
--- test/test.js
+++ test/test.js
@@ -1,47 +1,132 @@
 const supertest = require('supertest')
 const chai = require('chai')
 const app = require('../src/index')
+const joi = require('joi')
+const joiAssert = require('joi-assert')
 const should = chai.should();
 const User = require('../src/models/UsuarioSchema')
 
 describe('testes', function () {
 
-    it('retornar atividades', function (done) {
-        this.timeout(10000);
+    it('criar atividade corretamente', function (done) {
+        this.timeout(20000);
+
+        const jeison = joi.object({
+            _id: joi.any(),
+            nome: joi.string(),
+            data: joi.date(),
+            descricao: joi.string(),
+            __v: 0
+        });
+
+        let exemplo = {
+            nome: "Doação de roupas",
+            data: "2019-12-09T17:00:00.000+00:00",
+            descricao: "Doar agasalhos"
+        }
+
         supertest(app)
-            .get('/api/home')
+            .post('/api/ativ/')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+            .send(exemplo)
             .end(function (err, res) {
                 if (err) {
                     done(err);
                 }
-
+                joiAssert(res.body, jeison)
                 done();
             })
     })
 
     it('não retornar rota não criada', function (done) {
-        this.timeout(10000)
+        this.timeout(20000)
         supertest(app)
             .get('/api/atividade')
-            .expect(404,done)
+            .expect(404, done)
     });
 
-    it('retornar atividade cadastrada', function (done) {
-        this.timeout(10000);
+    it('criar usuario', function (done) {
+        this.timeout(20000);
+
+        const jeison = joi.object({
+            _id: joi.any(),
+            login: joi.string(),
+            senha: joi.string(),
+            __v: 0
+        });
+
+        let exemplo = {
+            login: "testando",
+            senha: "testando"
+        }
+
         supertest(app)
-            .get('/api/ativ/feijoada')
+            .post('/api/user/')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+            .send(exemplo)
             .end(function (err, res) {
                 if (err) {
                     done(err);
                 }
-
+                joiAssert(res.body, jeison)
                 done();
             })
     })
+
+    it('logar com usuario cadastrado', function (done) {
+        this.timeout(20000);
+
+        const jeison = joi.object({
+            _id: joi.any(),
+            login: joi.string(),
+            senha: joi.string()
+        });
+
+        let exemplo = {
+            login: "felipe",
+            senha: "felipe"
+        }
+
+        supertest(app)
+            .post('/api/user/felipe')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .send(exemplo)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                joiAssert(res.body, jeison)
+                done();
+            })
+    })
+
+    it('não logar com usuario nao cadastrado', function (done) {
+        this.timeout(20000);
+
+        let exemplo = {
+            login: "paulo",
+            senha: "paulo"
+        }
+
+        supertest(app)
+            .post('/api/user/paulo')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .send(exemplo)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                }
+                joiAssert(res.body, null)
+                done();
+            })
+    })
+
 })
